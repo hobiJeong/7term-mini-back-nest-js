e99d9c7ef11b4b0b78d99df293341737845a51e8
hobiJeong <jjb26433@naver.com>
2024-03-20 01:58:51 +0900

    feat: create api for create board comment

--- src/apis/board-comments/controllers/board-comments.controller.ts
+++ src/apis/board-comments/controllers/board-comments.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { Payload } from '@src/apis/auth/constants/payload.interface';
+import { JwtAccessTokenGuard } from '@src/apis/auth/jwt/guards/jwt-access-token.guard';
+import { BoardCommentDto } from '@src/apis/board-comments/dto/board-comment.dto';
+import { CreateBoardCommentRequestBodyDto } from '@src/apis/board-comments/dto/create-board-comment-request-body.dto';
+import { BoardCommentsService } from '@src/apis/board-comments/services/board-comments.service';
+import { User } from '@src/common/decorators/user.decorator';
+import { ParsePositiveIntPipe } from '@src/common/pipes/parse-positive-int.pipe';
+import { RESPONSE_KEY } from '@src/interceptors/response-transformer-interceptor/constants/response-key.enum';
+import { SetResponse } from '@src/interceptors/response-transformer-interceptor/decorators/set-response.decorator';
 
-@Controller('board-comments')
-export class BoardCommentsController {}
+@Controller('boards/:boardId/board-comments')
+export class BoardCommentsController {
+  constructor(private readonly boardCommentsService: BoardCommentsService) {}
+
+  @Post()
+  @UseGuards(JwtAccessTokenGuard)
+  @SetResponse(RESPONSE_KEY.Comment)
+  create(
+    @User() user: Payload,
+    @Param('boardId', ParsePositiveIntPipe) boardId: number,
+    @Body() createBoardCommentRequestBodyDto: CreateBoardCommentRequestBodyDto,
+  ): Promise<BoardCommentDto> {
+    return this.boardCommentsService.create(
+      user.id,
+      boardId,
+      createBoardCommentRequestBodyDto,
+    );
+  }
+}
