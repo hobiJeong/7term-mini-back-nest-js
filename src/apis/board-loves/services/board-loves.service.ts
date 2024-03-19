411508ebd3e25ea7dafa88976d602eac620613c2
hobiJeong <jjb26433@naver.com>
2024-03-20 00:30:43 +0900

    feat: create board love method

--- src/apis/board-loves/services/board-loves.service.ts
+++ src/apis/board-loves/services/board-loves.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { BoardLoveRepository } from '@src/apis/board-loves/repositories/board-love.repository';
+import { BoardsService } from '@src/apis/boards/services/boards.service';
+import { BoardLove } from '@src/entities/BoardLove';
 
 @Injectable()
 export class BoardLovesService {
-  constructor(private readonly) {}
+  constructor(
+    private readonly boardLoveRepository: BoardLoveRepository,
+    private readonly boardsService: BoardsService,
+  ) {}
 
-  create(userId: number, boardId: number) {}
+  async create(
+    userId: number,
+    boardId: number,
+  ): Promise<Pick<BoardLove, 'userId' | 'boardId'>> {
+    const existBoard = await this.boardsService.findOneOrNotFound(boardId);
+
+    const isBoardLove = await this.boardLoveRepository.existsBy({
+      boardId: existBoard.id,
+      userId,
+    });
+
+    if (isBoardLove) {
+      throw new ConflictException('Likes already exist on the post.');
+    }
+
+    return this.boardLoveRepository.save(
+      {
+        userId,
+        boardId: existBoard.id,
+      },
+      { reload: false },
+    );
+  }
 }
