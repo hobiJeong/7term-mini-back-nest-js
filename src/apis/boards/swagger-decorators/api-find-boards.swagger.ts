5641a4317123ae0e176a5347fddb48df3da0ee6b
hobiJeong <jjb26433@naver.com>
2024-03-20 00:27:29 +0900

    doc: 400번 에러 스키마 추가

--- src/apis/boards/swagger-decorators/api-find-boards.swagger.ts
+++ src/apis/boards/swagger-decorators/api-find-boards.swagger.ts
@@ -1,6 +1,9 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiExtraModels, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { FindBoardsPaginationResponseDto } from '@src/apis/boards/dto/find-boards-pagination-response.dto';
+import { COMMON_ERROR_HTTP_STATUS_CODE } from '@src/common/constants/common-error-http-status-code.enum';
+import { COMMON_ERROR_HTTP_STATUS_MESSAGE } from '@src/common/constants/common-error-http-status-message.enum';
+import { ErrorSwaggerBuilder } from '@src/common/dto/error-swagger.builder';
 
 export const ApiFindBoards = (summary: string) => {
   return applyDecorators(
@@ -11,6 +14,10 @@ export const ApiFindBoards = (summary: string) => {
       description: '성공적으로 게시글 전체 조회',
       type: FindBoardsPaginationResponseDto,
     }),
+    ErrorSwaggerBuilder(
+      COMMON_ERROR_HTTP_STATUS_CODE.BAD_REQUEST,
+      COMMON_ERROR_HTTP_STATUS_MESSAGE.BAD_REQUEST,
+    ),
     ApiExtraModels(FindBoardsPaginationResponseDto),
   );
 };
