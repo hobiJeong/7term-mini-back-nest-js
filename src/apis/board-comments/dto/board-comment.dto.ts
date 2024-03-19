f7102496ef995aa5758d734094fcf2be7f0d89b2
hobiJeong <jjb26433@naver.com>
2024-03-20 01:58:05 +0900

    feat: create board comment dto

---
+++ src/apis/board-comments/dto/board-comment.dto.ts
@@ -0,0 +1,42 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { BaseDto } from '@src/common/dto/base.dto';
+import { BoardComment } from '@src/entities/BoardComment';
+import { Exclude } from 'class-transformer';
+
+export class BoardCommentDto
+  extends BaseDto
+  implements
+    Pick<
+      BoardComment,
+      'id' | 'boardId' | 'userId' | 'content' | 'createdAt' | 'deletedAt'
+    >
+{
+  @ApiProperty({
+    description: '게시글 고유 ID',
+    format: 'integer',
+    minimum: 1,
+  })
+  boardId: number;
+
+  @ApiProperty({
+    description: '댓글 작성자 고유 ID',
+    format: 'integer',
+    minimum: 1,
+  })
+  userId: number;
+
+  @ApiProperty({
+    description: '댓글 본문',
+    minLength: 1,
+  })
+  content: string;
+
+  @Exclude()
+  deletedAt: Date;
+
+  constructor(boardCommentDto: Partial<BoardCommentDto> = {}) {
+    super();
+
+    Object.assign(this, boardCommentDto);
+  }
+}
