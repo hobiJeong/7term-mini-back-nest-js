ee1c44bab1d05596aa4c42b57cb4c2d08f4c3739
hobiJeong <jjb26433@naver.com>
2024-03-19 15:44:53 +0900

    refactor: set type

--- src/common/dto/internal-server-error.builder.ts
+++ src/common/dto/internal-server-error.builder.ts
@@ -3,7 +3,8 @@ import { ApiResponse } from '@nestjs/swagger';
 import { COMMON_ERROR_HTTP_STATUS_CODE } from '@src/common/constants/common-error-http-status-code.enum';
 import { COMMON_ERROR_HTTP_STATUS_MESSAGE } from '@src/common/constants/common-error-http-status-message.enum';
 
-export const InternalServerErrorSwaggerBuilder = () => {
+export const InternalServerErrorSwaggerBuilder = (): ClassDecorator &
+  MethodDecorator => {
   return applyDecorators(
     ApiResponse({
       status: COMMON_ERROR_HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR,
