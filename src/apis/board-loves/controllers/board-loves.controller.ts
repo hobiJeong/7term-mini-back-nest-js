213ccf8f65ff60de115979a4d87c164b3046d7b8
hobiJeong <jjb26433@naver.com>
2024-03-20 00:56:21 +0900

    doc: 게시글 좋아요 삭제 api docs 생성 및 적용

--- src/apis/board-loves/controllers/board-loves.controller.ts
+++ src/apis/board-loves/controllers/board-loves.controller.ts
@@ -12,6 +12,7 @@ import { Payload } from '@src/apis/auth/constants/payload.interface';
 import { JwtAccessTokenGuard } from '@src/apis/auth/jwt/guards/jwt-access-token.guard';
 import { BoardLovesService } from '@src/apis/board-loves/services/board-loves.service';
 import { ApiCreateBoardLove } from '@src/apis/board-loves/swagger-decorators/api-create-board-love.swagger';
+import { ApiDeleteBoardLove } from '@src/apis/board-loves/swagger-decorators/api-delete-board-love.swagger';
 import { User } from '@src/common/decorators/user.decorator';
 import { InternalServerErrorSwaggerBuilder } from '@src/common/dto/internal-server-error.builder';
 import { ParsePositiveIntPipe } from '@src/common/pipes/parse-positive-int.pipe';
@@ -19,6 +20,7 @@ import { BoardLove } from '@src/entities/BoardLove';
 
 @ApiTags('board-love')
 @InternalServerErrorSwaggerBuilder()
+@UseGuards(JwtAccessTokenGuard)
 @Controller('boards/:boardId/board-loves')
 export class BoardLovesController {
   constructor(private readonly boardLovesService: BoardLovesService) {}
@@ -26,7 +28,6 @@ export class BoardLovesController {
   @Post()
   @ApiCreateBoardLove('게시글 좋아요 생성 API')
   @HttpCode(HttpStatus.NO_CONTENT)
-  @UseGuards(JwtAccessTokenGuard)
   create(
     @Param('boardId', ParsePositiveIntPipe) boardId: number,
     @User() user: Payload,
@@ -35,8 +36,8 @@ export class BoardLovesController {
   }
 
   @Delete()
+  @ApiDeleteBoardLove('게시글 좋아요 삭제 API')
   @HttpCode(HttpStatus.NO_CONTENT)
-  @UseGuards(JwtAccessTokenGuard)
   async delete(
     @Param('boardId', ParsePositiveIntPipe) boardId: number,
     @User() user: Payload,
---
+++ src/apis/board-loves/swagger-decorators/api-delete-board-love.swagger.ts
@@ -0,0 +1,37 @@
+import { applyDecorators } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiNoContentResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
+import { COMMON_ERROR_HTTP_STATUS_CODE } from '@src/common/constants/common-error-http-status-code.enum';
+import { COMMON_ERROR_HTTP_STATUS_MESSAGE } from '@src/common/constants/common-error-http-status-message.enum';
+import { ErrorSwaggerBuilder } from '@src/common/dto/error-swagger.builder';
+
+export const ApiDeleteBoardLove = (summary: string) => {
+  return applyDecorators(
+    ApiOperation({
+      summary,
+    }),
+    ApiBearerAuth('access-token'),
+    ApiNoContentResponse({
+      description: '성공적으로 게시글 좋아요 삭제',
+    }),
+    ErrorSwaggerBuilder(
+      COMMON_ERROR_HTTP_STATUS_CODE.FORBIDDEN,
+      COMMON_ERROR_HTTP_STATUS_MESSAGE.FORBIDDEN,
+    ),
+    ErrorSwaggerBuilder(
+      COMMON_ERROR_HTTP_STATUS_CODE.UNAUTHORIZED,
+      COMMON_ERROR_HTTP_STATUS_MESSAGE.UNAUTHORIZED,
+    ),
+    ErrorSwaggerBuilder(
+      COMMON_ERROR_HTTP_STATUS_CODE.NOT_FOUND,
+      COMMON_ERROR_HTTP_STATUS_MESSAGE.NOT_FOUND,
+    ),
+    ErrorSwaggerBuilder(
+      COMMON_ERROR_HTTP_STATUS_CODE.CONFLICT,
+      COMMON_ERROR_HTTP_STATUS_MESSAGE.CONFLICT,
+    ),
+  );
+};
