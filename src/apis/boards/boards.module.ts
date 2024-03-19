2a24db234bccb7b5d7d82ee149562ddb021b19f9
hobiJeong <jjb26433@naver.com>
2024-03-19 16:57:31 +0900

    feat: create boards module

--- src/apis/boards/boards.module.ts
+++ src/apis/boards/boards.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { BoardsController } from '@src/apis/boards/controllers/boards.controller';
+import { BoardRepository } from '@src/apis/boards/repositories/board.repository';
+import { BoardsService } from '@src/apis/boards/services/boards.service';
+import { CategoriesModule } from '@src/apis/categories/categories.module';
+import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmExModule.forCustomRepository([BoardRepository]),
+    CategoriesModule,
+  ],
+  controllers: [BoardsController],
+  providers: [BoardsService],
+})
 export class BoardsModule {}
