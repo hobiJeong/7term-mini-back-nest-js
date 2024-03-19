774b2a6da3b1d5d8fd0fb49eebb4b0f2e0821eef
hobiJeong <jjb26433@naver.com>
2024-03-20 02:05:09 +0900

    doc: 댓글 생성 api doc 생성

---
+++ src/apis/board-comments/swagger-decorators/api-create-board-comment.swagger.ts
@@ -0,0 +1,43 @@
+import { applyDecorators } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiExtraModels,
+  ApiOperation,
+  getSchemaPath,
+} from '@nestjs/swagger';
+import { BoardCommentDto } from '@src/apis/board-comments/dto/board-comment.dto';
+import { COMMON_ERROR_HTTP_STATUS_CODE } from '@src/common/constants/common-error-http-status-code.enum';
+import { COMMON_ERROR_HTTP_STATUS_MESSAGE } from '@src/common/constants/common-error-http-status-message.enum';
+import { ErrorSwaggerBuilder } from '@src/common/dto/error-swagger.builder';
+import { RESPONSE_KEY } from '@src/interceptors/response-transformer-interceptor/constants/response-key.enum';
+
+export const ApiCreateBoardComment = (summary: string) => {
+  return applyDecorators(
+    ApiOperation({
+      summary,
+    }),
+    ApiCreatedResponse({
+      description: '성공적으로 댓글 생성',
+      schema: {
+        properties: {
+          [RESPONSE_KEY.Comment]: {
+            $ref: getSchemaPath(BoardCommentDto),
+          },
+        },
+      },
+    }),
+    ErrorSwaggerBuilder(
+      COMMON_ERROR_HTTP_STATUS_CODE.BAD_REQUEST,
+      COMMON_ERROR_HTTP_STATUS_MESSAGE.BAD_REQUEST,
+    ),
+    ErrorSwaggerBuilder(
+      COMMON_ERROR_HTTP_STATUS_CODE.UNAUTHORIZED,
+      COMMON_ERROR_HTTP_STATUS_MESSAGE.UNAUTHORIZED,
+    ),
+    ErrorSwaggerBuilder(
+      COMMON_ERROR_HTTP_STATUS_CODE.NOT_FOUND,
+      COMMON_ERROR_HTTP_STATUS_MESSAGE.NOT_FOUND,
+    ),
+    ApiExtraModels(BoardCommentDto),
+  );
+};
