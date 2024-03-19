f5656cbf64d3f5f06d36d4a78df5af05269b3349
hobiJeong <jjb26433@naver.com>
2024-03-19 23:35:45 +0900

    modify: import HelpersModule

--- src/apis/boards/boards.module.ts
+++ src/apis/boards/boards.module.ts
@@ -4,11 +4,13 @@ import { BoardRepository } from '@src/apis/boards/repositories/board.repository'
 import { BoardsService } from '@src/apis/boards/services/boards.service';
 import { CategoriesModule } from '@src/apis/categories/categories.module';
 import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
+import { HelpersModule } from '@src/helpers/helpers.module';
 
 @Module({
   imports: [
     TypeOrmExModule.forCustomRepository([BoardRepository]),
     CategoriesModule,
+    HelpersModule,
   ],
   controllers: [BoardsController],
   providers: [BoardsService],
