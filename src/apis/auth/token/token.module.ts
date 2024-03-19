af3cdd340fc5ddddf86b77296d4cb9de4807594c
hobiJeong <jjb26433@naver.com>
2024-03-19 16:57:07 +0900

    feat: imports PassportModule and apply strategies in providers array

--- src/apis/auth/token/token.module.ts
+++ src/apis/auth/token/token.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { JwtModuleOptionsFactory } from '@src/apis/auth/jwt/factories/jwt-module-options.factory';
+import { JwtAccessTokenStrategy } from '@src/apis/auth/jwt/strategies/jwt-access-token.strategy';
+import { JwtRefreshTokenStrategy } from '@src/apis/auth/jwt/strategies/jwt-refresh-token.strategy';
 import { TokenRepository } from '@src/apis/auth/token/repositories/token.repository';
 import { TokenService } from '@src/apis/auth/token/services/token.service';
+import { UsersModule } from '@src/apis/users/users.module';
 import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
 
 @Module({
@@ -10,9 +14,16 @@ import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
     JwtModule.registerAsync({
       useClass: JwtModuleOptionsFactory,
     }),
+    PassportModule,
     TypeOrmExModule.forCustomRepository([TokenRepository]),
+    UsersModule,
   ],
-  providers: [TokenService, JwtModuleOptionsFactory],
+  providers: [
+    TokenService,
+    JwtModuleOptionsFactory,
+    JwtAccessTokenStrategy,
+    JwtRefreshTokenStrategy,
+  ],
   exports: [TokenService],
 })
 export class TokenModule {}
