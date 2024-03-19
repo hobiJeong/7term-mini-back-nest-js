50469ba7e0c2862dfe27485c03e137ebb277f3c5
hobiJeong <jjb26433@naver.com>
2024-03-20 02:32:32 +0900

    feat: import helpers module

--- src/apis/board-comments/board-comments.module.ts
+++ src/apis/board-comments/board-comments.module.ts
@@ -4,11 +4,13 @@ import { BoardCommentsService } from './services/board-comments.service';
 import { BoardsModule } from '@src/apis/boards/boards.module';
 import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
 import { BoardCommentRepository } from '@src/apis/board-comments/repositories/board-comment.repository';
+import { HelpersModule } from '@src/helpers/helpers.module';
 
 @Module({
   imports: [
     TypeOrmExModule.forCustomRepository([BoardCommentRepository]),
     BoardsModule,
+    HelpersModule,
   ],
   controllers: [BoardCommentsController],
   providers: [BoardCommentsService],
