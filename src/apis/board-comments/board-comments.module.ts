a0c4b39c296bbe7ed1231aa0ae13ce996402d82e
hobiJeong <jjb26433@naver.com>
2024-03-20 01:39:53 +0900

    feat: import Type orm ex module and boards module

--- src/apis/board-comments/board-comments.module.ts
+++ src/apis/board-comments/board-comments.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BoardCommentsController } from './controllers/board-comments.controller';
 import { BoardCommentsService } from './services/board-comments.service';
+import { BoardsModule } from '@src/apis/boards/boards.module';
+import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
+import { BoardRepository } from '@src/apis/boards/repositories/board.repository';
 
 @Module({
+  imports: [
+    TypeOrmExModule.forCustomRepository([BoardRepository]),
+    BoardsModule,
+  ],
   controllers: [BoardCommentsController],
   providers: [BoardCommentsService],
 })
