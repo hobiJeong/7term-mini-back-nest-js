355a05173a3a2ead5834c1d0292eb2dbc797419a
hobiJeong <jjb26433@naver.com>
2024-03-20 02:32:20 +0900

    feat: 댓글 페이지네이션 api 추가

--- src/apis/board-comments/services/board-comments.service.ts
+++ src/apis/board-comments/services/board-comments.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { BoardCommentDto } from '@src/apis/board-comments/dto/board-comment.dto';
 import { CreateBoardCommentRequestBodyDto } from '@src/apis/board-comments/dto/create-board-comment-request-body.dto';
+import { FindBoardCommentsQueryDto } from '@src/apis/board-comments/dto/find-board-comments-query.dto';
 import { BoardCommentRepository } from '@src/apis/board-comments/repositories/board-comment.repository';
 import { BoardsService } from '@src/apis/boards/services/boards.service';
+import { BoardComment } from '@src/entities/BoardComment';
+import { QueryHelper } from '@src/helpers/providers/query.helper';
 
 @Injectable()
 export class BoardCommentsService {
+  private readonly LIKE_SEARCH_FIELD: readonly (keyof Pick<
+    BoardComment,
+    'content'
+  >)[] = ['content'];
+
   constructor(
     private readonly boardCommentRepository: BoardCommentRepository,
     private readonly boardsService: BoardsService,
+    private readonly queryHelper: QueryHelper,
   ) {}
 
   async create(
@@ -27,5 +36,36 @@ export class BoardCommentsService {
     await this.boardCommentRepository.save(newBoardComment);
 
     return new BoardCommentDto(newBoardComment);
+  }
+
+  async findByPagination(
+    boardId: number,
+    findBoardCommentsQueryDto: FindBoardCommentsQueryDto,
+  ): Promise<[BoardComment[], number]> {
+    const { orderField, sortOrder, page, pageSize, ...filter } =
+      findBoardCommentsQueryDto;
+
+    const existBoard = await this.boardsService.findOneOrNotFound(boardId);
+
+    const skip = (page - 1) * pageSize;
+
+    filter['boardId'] = existBoard.id;
+
+    const where = this.queryHelper.buildWherePropForFind(
+      filter,
+      this.LIKE_SEARCH_FIELD,
+    );
+
+    const order = this.queryHelper.buildOrderByPropForFind(
+      orderField,
+      sortOrder,
+    );
+
+    return this.boardCommentRepository.findAndCount({
+      where,
+      order,
+      skip,
+      take: pageSize,
+    });
   }
 }
