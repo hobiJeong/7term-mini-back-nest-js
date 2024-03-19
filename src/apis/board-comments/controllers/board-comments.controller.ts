1cc396a31cbe50a1f7f44d72e8dd10b36ed51658
hobiJeong <jjb26433@naver.com>
2024-03-20 02:44:18 +0900

    feat: create find board comments pagination api and doc

--- src/apis/board-comments/controllers/board-comments.controller.ts
+++ src/apis/board-comments/controllers/board-comments.controller.ts
@@ -13,14 +13,17 @@ import { JwtAccessTokenGuard } from '@src/apis/auth/jwt/guards/jwt-access-token.
 import { BoardCommentDto } from '@src/apis/board-comments/dto/board-comment.dto';
 import { CreateBoardCommentRequestBodyDto } from '@src/apis/board-comments/dto/create-board-comment-request-body.dto';
 import { FindBoardCommentsQueryDto } from '@src/apis/board-comments/dto/find-board-comments-query.dto';
+import { FindBoardCommentsResponseDto } from '@src/apis/board-comments/dto/find-board-comments-response.dto';
 import { BoardCommentsService } from '@src/apis/board-comments/services/board-comments.service';
 import { ApiCreateBoardComment } from '@src/apis/board-comments/swagger-decorators/api-create-board-comment.swagger';
+import { ApiFindBoardComments } from '@src/apis/board-comments/swagger-decorators/api-find-board-comments.swagger';
 import { User } from '@src/common/decorators/user.decorator';
 import { InternalServerErrorSwaggerBuilder } from '@src/common/dto/internal-server-error.builder';
 import { ParsePositiveIntPipe } from '@src/common/pipes/parse-positive-int.pipe';
 import { RESPONSE_KEY } from '@src/interceptors/response-transformer-interceptor/constants/response-key.enum';
 import { ResponseType } from '@src/interceptors/response-transformer-interceptor/constants/response-type.enum';
 import { SetResponse } from '@src/interceptors/response-transformer-interceptor/decorators/set-response.decorator';
+import { plainToInstance } from 'class-transformer';
 
 @ApiTags('board-comments')
 @InternalServerErrorSwaggerBuilder()
@@ -44,15 +47,22 @@ export class BoardCommentsController {
     );
   }
 
+  @ApiFindBoardComments('댓글 전체 조회 API')
   @Get()
   @SetResponse(RESPONSE_KEY.Comments, ResponseType.Pagination)
-  find(
+  async find(
     @Param('boardId', ParsePositiveIntPipe) boardId: number,
     @Query() findBoardCommentsQueryDto: FindBoardCommentsQueryDto,
-  ) {
-    return this.boardCommentsService.findByPagination(
-      boardId,
-      findBoardCommentsQueryDto,
-    );
+  ): Promise<[FindBoardCommentsResponseDto[], number]> {
+    const [boardComments, totalCount] =
+      await this.boardCommentsService.findByPagination(
+        boardId,
+        findBoardCommentsQueryDto,
+      );
+
+    return [
+      plainToInstance(FindBoardCommentsResponseDto, boardComments),
+      totalCount,
+    ];
   }
 }
---
+++ src/apis/board-comments/swagger-decorators/api-find-board-comments.swagger.ts
@@ -0,0 +1,27 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { FindBoardCommentsPaginationResponseDto } from '@src/apis/board-comments/dto/find-board-comments-paignation-response.dto';
+import { COMMON_ERROR_HTTP_STATUS_CODE } from '@src/common/constants/common-error-http-status-code.enum';
+import { COMMON_ERROR_HTTP_STATUS_MESSAGE } from '@src/common/constants/common-error-http-status-message.enum';
+import { ErrorSwaggerBuilder } from '@src/common/dto/error-swagger.builder';
+
+export const ApiFindBoardComments = (summary: string) => {
+  return applyDecorators(
+    ApiOperation({
+      summary,
+    }),
+    ApiOkResponse({
+      description: '성공적으로 댓글 전체 조회(pagination)',
+      type: FindBoardCommentsPaginationResponseDto,
+    }),
+    ErrorSwaggerBuilder(
+      COMMON_ERROR_HTTP_STATUS_CODE.BAD_REQUEST,
+      COMMON_ERROR_HTTP_STATUS_MESSAGE.BAD_REQUEST,
+    ),
+    ErrorSwaggerBuilder(
+      COMMON_ERROR_HTTP_STATUS_CODE.NOT_FOUND,
+      COMMON_ERROR_HTTP_STATUS_MESSAGE.NOT_FOUND,
+    ),
+    ApiExtraModels(FindBoardCommentsPaginationResponseDto),
+  );
+};
