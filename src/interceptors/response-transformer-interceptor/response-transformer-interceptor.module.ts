a00a3b9e88cb3144e589d4f1441ebb404f002000
hobiJeong <jjb26433@naver.com>
2024-03-19 23:30:19 +0900

    feat: create ResponseBuilder for pagination response

--- src/interceptors/response-transformer-interceptor/response-transformer-interceptor.module.ts
+++ src/interceptors/response-transformer-interceptor/response-transformer-interceptor.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ResponseBuilder } from '@src/interceptors/response-transformer-interceptor/builders/response-transformer.builder';
 import { ResponseTransformerInterceptor } from '@src/interceptors/response-transformer-interceptor/response-transformer.interceptor';
 
 @Module({
-  providers: [ResponseTransformerInterceptor],
+  providers: [ResponseTransformerInterceptor, ResponseBuilder],
 })
 export class ResponseTransformerInterceptorModule {}
