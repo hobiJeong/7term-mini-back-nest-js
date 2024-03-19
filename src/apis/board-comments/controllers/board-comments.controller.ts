0f178a524a34d503429036d0c1fc2a633dbb6e56
hobiJeong <jjb26433@naver.com>
2024-03-20 02:05:15 +0900

    doc: 댓글 생성 api doc 적용

--- src/apis/board-comments/controllers/board-comments.controller.ts
+++ src/apis/board-comments/controllers/board-comments.controller.ts
@@ -1,18 +1,24 @@
 import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { Payload } from '@src/apis/auth/constants/payload.interface';
 import { JwtAccessTokenGuard } from '@src/apis/auth/jwt/guards/jwt-access-token.guard';
 import { BoardCommentDto } from '@src/apis/board-comments/dto/board-comment.dto';
 import { CreateBoardCommentRequestBodyDto } from '@src/apis/board-comments/dto/create-board-comment-request-body.dto';
 import { BoardCommentsService } from '@src/apis/board-comments/services/board-comments.service';
+import { ApiCreateBoardComment } from '@src/apis/board-comments/swagger-decorators/api-create-board-comment.swagger';
 import { User } from '@src/common/decorators/user.decorator';
+import { InternalServerErrorSwaggerBuilder } from '@src/common/dto/internal-server-error.builder';
 import { ParsePositiveIntPipe } from '@src/common/pipes/parse-positive-int.pipe';
 import { RESPONSE_KEY } from '@src/interceptors/response-transformer-interceptor/constants/response-key.enum';
 import { SetResponse } from '@src/interceptors/response-transformer-interceptor/decorators/set-response.decorator';
 
+@ApiTags('board-comments')
+@InternalServerErrorSwaggerBuilder()
 @Controller('boards/:boardId/board-comments')
 export class BoardCommentsController {
   constructor(private readonly boardCommentsService: BoardCommentsService) {}
 
+  @ApiCreateBoardComment('댓글 생성 API')
   @Post()
   @UseGuards(JwtAccessTokenGuard)
   @SetResponse(RESPONSE_KEY.Comment)
