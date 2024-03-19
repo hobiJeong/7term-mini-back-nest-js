5178ee783b92a887863ec138a8ab634c3fa2683c
hobiJeong <jjb26433@naver.com>
2024-03-19 23:28:28 +0900

    setting: set global prefix api

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get<Reflector>(Reflector)),
     app.get<ResponseTransformerInterceptor>(ResponseTransformerInterceptor),
