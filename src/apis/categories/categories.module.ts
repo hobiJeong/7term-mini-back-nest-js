70fd56b43d9a6eab6729474b1b8dc49b99d373af
hobiJeong <jjb26433@naver.com>
2024-03-20 14:49:26 +0900

    feat: controllers 배열에 컨트롤러 push

--- src/apis/categories/categories.module.ts
+++ src/apis/categories/categories.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CategoriesController } from '@src/apis/categories/controllers/categories.controller';
 import { CategoryRepository } from '@src/apis/categories/repositories/category.repository';
 import { CategoriesService } from '@src/apis/categories/services/categories.service';
 import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
 
 @Module({
   imports: [TypeOrmExModule.forCustomRepository([CategoryRepository])],
+  controllers: [CategoriesController],
   providers: [CategoriesService],
   exports: [CategoriesService],
 })
