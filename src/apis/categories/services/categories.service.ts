2cd4f5bd64348728ed8e95db99080e62d3919ea9
hobiJeong <jjb26433@naver.com>
2024-03-20 14:47:31 +0900

    feat: 카테고리 생성 메서드 생성

--- src/apis/categories/services/categories.service.ts
+++ src/apis/categories/services/categories.service.ts
@@ -1,10 +1,37 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CategoryDto } from '@src/apis/categories/dto/category.dto';
+import { CreateCategoryRequestBodyDto } from '@src/apis/categories/dto/create-category-request-body.dto';
 import { CategoryRepository } from '@src/apis/categories/repositories/category.repository';
 
 @Injectable()
 export class CategoriesService {
   constructor(private readonly categoryRepository: CategoryRepository) {}
+
+  async create(
+    createCategoryRequestBodyDto: CreateCategoryRequestBodyDto,
+  ): Promise<CategoryDto> {
+    const { name } = createCategoryRequestBodyDto;
+
+    const isExistCategory = await this.categoryRepository.existsBy({
+      name,
+    });
+
+    if (isExistCategory) {
+      throw new ConflictException('중복');
+    }
+
+    const newCategory = this.categoryRepository.create({
+      name,
+    });
+
+    await this.categoryRepository.save(newCategory);
+
+    return new CategoryDto(newCategory);
+  }
 
   async findOneOrNotFound(categoryId: number): Promise<CategoryDto> {
     const category = await this.categoryRepository.findOneBy({
