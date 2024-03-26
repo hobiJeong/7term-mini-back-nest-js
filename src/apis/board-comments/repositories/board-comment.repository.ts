4ed7e494db198004e8603b4934e2691495bb339d
hobiJeong <jjb26433@naver.com>
2024-03-26 17:40:11 +0900

    feat: 인터페이스를 구현한 구현체

--- src/apis/board-comments/repositories/board-comment.repository.ts
+++ src/apis/board-comments/repositories/board-comment.repository.ts
@@ -1,6 +1,56 @@
+import { CreateBoardCommentDto } from '@src/apis/board-comments/dto/create-board-comment.dto';
+import { IBoardCommentRepository } from '@src/apis/board-comments/repositories/iboard-comment.repository';
+import { FindByPaginationParameter } from '@src/common/constants/find-by-pagination-parameter.interface';
 import { CustomRepository } from '@src/core/type-orm/decorators/custom-repository.decorator';
 import { BoardComment } from '@src/entities/BoardComment';
-import { Repository } from 'typeorm';
+import { FindOneOptions, Repository, UpdateResult } from 'typeorm';
 
 @CustomRepository(BoardComment)
-export class BoardCommentRepository extends Repository<BoardComment> {}
+export class BoardCommentRepository
+  extends Repository<BoardComment>
+  implements IBoardCommentRepository
+{
+  saveBoard(
+    createBoardCommentDto: CreateBoardCommentDto,
+  ): Promise<BoardComment> {
+    return this.save(
+      this.create({
+        ...new CreateBoardCommentDto({ ...createBoardCommentDto }),
+      }),
+    );
+  }
+
+  findByPagination(
+    boardId: number,
+    findByPaginationParameter: FindByPaginationParameter<
+      Pick<BoardComment, 'id' | 'userId' | 'content'>
+    >,
+  ): Promise<[BoardComment[], number]> {
+    const { where } = findByPaginationParameter;
+
+    return this.findAndCount({
+      ...findByPaginationParameter,
+      where: {
+        ...where,
+        boardId,
+      },
+    });
+  }
+
+  findOneBoardComment(
+    options: FindOneOptions<BoardComment>,
+  ): Promise<BoardComment> {
+    return this.findOne(options);
+  }
+
+  deleteBoardComment(
+    boardComment: Pick<BoardComment, 'id' | 'boardId'> & Partial<BoardComment>,
+  ): Promise<UpdateResult> {
+    const { id, boardId } = boardComment;
+
+    return this.update(
+      { id, boardId },
+      { ...boardComment, deletedAt: new Date() },
+    );
+  }
+}
